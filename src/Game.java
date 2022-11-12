5b22024fe96c85c6b44b4c8cd0ae86645b5c1581
Preston Edwards <prestonedwards@vt.edu>
2022-11-12 14:37:26 -0500

    Game.java and leaderboard.java work as intended. Now just need to uncomment some randomization lines

--- src/Game.java
+++ src/Game.java
@@ -23,7 +23,13 @@ public class Game {
     public static void main(String args[]) throws IOException {
         Game test = new Game();
         test.getNewTweet();
+        test.addScore();
+        test.board.update(test.currentScore, "Preston");
         System.out.println(test.tweet);
+        System.out.println(test.board);
+        test.addScore();
+        test.board.update(test.currentScore, "Zach");
+        System.out.println(test.board);
     }
 
     // ~ Default Constructor ...........................................................
